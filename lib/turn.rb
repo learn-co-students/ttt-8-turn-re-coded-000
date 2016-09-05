0fa0478993e037d49aa17e019230cd85c94dadc5
shereen messi <shereenmessi@hotmail.com>
2016-09-05 10:31:52 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,61 @@
+def display_board(board)
+puts  " #{board[0]} | #{board[1]} | #{board[2]} "
+puts  "-----------"
+puts  " #{board[3]} | #{board[4]} | #{board[5]} "
+puts  "-----------"
+puts  " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def input_to_index(position)
+  index=position.to_i-1
+  return index
+end
+
+
+def move(board, position, value="X")
+  index=input_to_index(position)
+
+  board[index] = value
+end
+
+
+def position_taken? (board,position)
+  if board[position]==" " || board[position]=="" || !board[position]
+    return false
+  elsif board[position]=="X" || board[position]=="O"
+    return true
+  end
+end
+
+def valid_move? (board,position)
+  if  position.to_i< 10 &&position.to_i>0
+    return ! position_taken?(board,position.to_i-1)
+  else
+    return false
+end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken? (board,position)
+  if board[position]==" " || board[position]=="" || !board[position]
+    return false
+  elsif board[position]=="X" || board[position]=="O"
+    return true
+  end
+end
+
+def turn(board)
+  display_board(board)
+  puts "Please enter 1-9:"
+
+  position=gets.strip
+  if valid_move?(board,position)
+      move(board,position)
+      display_board(board)
+    else
+      puts "Please enter 1-9:"
+      position=gets.strip
+      
+  end
+end
