b48c1ab13da858ebcc7f88b54809e04161c83d2a
dina aziz <dina.aziz100@yahoo.com>
2016-10-12 00:54:02 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,15 +7,12 @@ def display_board(board)
 end
 
 def valid_move?(board, position)
-  if position.to_i.between?(1,9)
-    if !position_taken?(board, position.to_i-1)
+  if position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
       true
     end
   end
 
   # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
-end
-
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
